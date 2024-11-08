44209237e695474deafbf36b35fde5916a603091
Zeng Peng <zeng.peng@hotmail.com>
2024-11-08 22:16:00 +0100

    Initial Commit

---
+++ caro/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  caro
+//
+//  Created by Zeng on 2024/11/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ caro/caroApp.swift
@@ -0,0 +1,17 @@
+//
+//  caroApp.swift
+//  caro
+//
+//  Created by Zeng on 2024/11/8.
+//
+
+import SwiftUI
+
+@main
+struct caroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
